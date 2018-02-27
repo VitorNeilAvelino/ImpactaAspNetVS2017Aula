8f6c344a73e9c89ac857c9d81660f1c361dd913c
vitor.avelino <vitor.avelino@concretesolutions.com.br>
2018-02-27 20:05:47 -0300

    GetRequestTeste

---
+++ Capitulo06.Http.Testes/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Capitulo06.Http.Testes")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Capitulo06.Http.Testes")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("368806a7-4351-49b9-974e-6cd1b13ca176")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Capitulo06.Http.Testes/RequestTeste.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Net;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Capitulo06.Http.Testes
+{
+    [TestClass]
+    public class HttpTeste
+    {
+        [TestMethod]
+        public void GetRequestTeste()
+        {
+            var request = (HttpWebRequest)WebRequest.Create("http://www.example.com");
+            request.Method = "GET";
+
+            //request.UserAgent = "Visual Studio";
+            //request.Date = DateTime.Now;
+
+            //request.ProtocolVersion = new Version("2.0"); //No momento, só há suporte para solicitações das versões HTTP/1.0 e HTTP/1.1.
+            //request.Headers.Add(HttpRequestHeader.UserAgent, "Visual Studio"); //O cabeçalho 'User-Agent' deve ser modificado com a propriedade ou o método adequado.
+            //request.CookieContainer = new CookieContainer();
+            //request.CookieContainer.Add(new Cookie("Tema", "azul")); //O parâmetro '{0}' não pode ser uma cadeia de caracteres vazia. Nome do parâmetro: cookie.Domain
+
+            Console.WriteLine(GetRequestToString(request));
+        }
+
+        public string GetRequestToString(HttpWebRequest request)
+        {
+            var requestLine = $"{request.Method} {request.RequestUri} HTTP/{request.ProtocolVersion}";
+            var headers = "";
+
+            foreach (var key in request.Headers.AllKeys)
+            {
+                headers += $"{key}: {request.Headers[key]}" + Environment.NewLine;
+            }
+
+            return requestLine + Environment.NewLine + headers;
+        }
+    }
+}
