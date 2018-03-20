408c7d26413836be6f22c16188cb6c74f841f979
VitorAvelinoCS <vitor.avelino@concrete.com.br>
2018-03-20 18:27:00 -0300

    Adicionar classe de exception personalizada

---
+++ Oficina.Dominio/FaturamentoException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Oficina.Dominio
+{
+    public class FaturamentoException : Exception
+    {
+        public FaturamentoException(string mensagem) : base(mensagem)
+        {
+            //base.Message = mensagem;
+        }
+    }
+}
