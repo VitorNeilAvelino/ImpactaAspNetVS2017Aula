1a028bfbb9cc423a22ecb44b0f7275886777cf72
VitorNeilAvelino <avelino.vitor@gmail.com>
2018-04-05 22:44:13 -0300

    Loja.Mvc

---
+++ Loja.Mvc/Controllers/ProdutosController.cs
@@ -0,0 +1,99 @@
+﻿using Loja.Dominio;
+using Loja.Mvc.Models;
+using Loja.Repositorios.SqlServer.EF.CodeFirst;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Loja.Mvc.Controllers
+{
+    public class ProdutosController : Controller
+    {
+        private LojaDbContext _db = new LojaDbContext();
+
+        // GET: Produtos
+        public ActionResult Index()
+        {
+            return View(Mapear(_db.Produtos.ToList()));
+        }
+
+        private List<ProdutoViewModel> Mapear(List<Produto> produtos)
+        {
+            throw new NotImplementedException();
+        }
+
+        // GET: Produtos/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Produtos/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Produtos/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Produtos/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Produtos/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Produtos/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Produtos/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ Loja.Mvc/Models/ProdutoViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Web.UI.WebControls;
+
+namespace Loja.Mvc.Models
+{
+    public class ProdutoViewModel
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Nome { get; set; }
+
+        [Required]        
+        public int CategoriaId { get; set; }
+
+        public string CategoriaNome { get; set; }
+
+        public List<ListItem> Categorias { get; set; }
+
+        [Required]
+        public decimal Preco { get; set; }
+
+        [Required]
+        public int QuantidadeEstoque { get; set; }
+
+        public bool Ativo { get; set; }
+    }
+}
