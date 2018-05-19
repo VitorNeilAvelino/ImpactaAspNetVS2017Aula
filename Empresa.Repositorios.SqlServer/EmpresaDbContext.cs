37e4c43c4879b264271d5e5d0204297b688b2d4b
VitorNeilAvelino <avelino.vitor@gmail.com>
2018-05-18 22:30:26 -0300

    ASP.NET Core

---
+++ Empresa.Repositorios.SqlServer/EmpresaDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Empresa.Dominio;
+using Microsoft.EntityFrameworkCore;
+
+namespace Empresa.Repositorios.SqlServer
+{
+    public class EmpresaDbContext : DbContext
+    {
+        public EmpresaDbContext(DbContextOptions options) : base(options)
+        {
+            Database.EnsureCreated();
+        }
+
+        public DbSet<Contato> Contatos { get; set; }
+    }
+}
