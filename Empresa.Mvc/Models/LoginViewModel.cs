5d958486682e019b9e03248c6e37494081a26f4c
Vitor Neil Avelino <avelino.vitor@gmail.com>
2018-05-26 14:50:44 -0300

    .NET Core

---
+++ Empresa.Mvc/Models/LoginViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Empresa.Mvc.Models
+{
+    public class LoginViewModel
+    {
+        [Required(ErrorMessage = "E-mail é obrigatório.")]
+        [DataType(DataType.EmailAddress)]
+        //[EmailAddress]
+        [Display(Name = "E-mail:")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Senha é obrigaória.")]
+        [DataType(DataType.Password)]
+        [Display(Name = "Senha:")]
+        public string Senha { get; set; }
+    }
+}
