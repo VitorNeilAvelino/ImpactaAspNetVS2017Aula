e6be62146599443c072386174704e337886d18bf
Vítor Neil Avelino <avelino.vitor@gmail.com>
2018-08-13 13:31:02 -0300

    Testes para ViagensOnline

---
+++ ViagensOnline.Repositorios.SqlServer/ViagensOnlineDbContext.cs
@@ -0,0 +1,23 @@
+﻿using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using ViagensOnline.Dominio;
+
+namespace ViagensOnline.Repositorios.SqlServer
+{
+    public class ViagensOnlineDbContext : DbContext
+    {
+        public ViagensOnlineDbContext() : base("viagensOnlineConnectionString")
+        {
+
+        }
+
+        public DbSet<Destino> Destinos { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            //base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
---
+++ ViagensOnline.Repositorios.SqlServerTests/ViagensOnlineDbContextTests.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ViagensOnline.Dominio;
+
+namespace ViagensOnline.Repositorios.SqlServer.Tests
+{
+    [TestClass()]
+    public class ViagensOnlineDbContextTests
+    {
+        private ViagensOnlineDbContext _db = new ViagensOnlineDbContext();
+
+        [TestMethod()]
+        public void InserirTeste()
+        {
+            var destino = new Destino();
+
+            destino.CaminhoFoto = "Paulista.png";
+            destino.Cidade = "São Paulo";
+            destino.Nome = "São Paulo";
+            destino.Pais = "Brasil";
+
+            _db.Destinos.Add(destino);
+
+            _db.SaveChanges();
+        }
+
+        [TestMethod]
+        public void AtualizarTeste()
+        {
+            var destino = _db.Destinos.Find(1);
+
+            destino.Pais = "Brazil";
+
+            _db.SaveChanges();
+        }
+
+        [TestMethod]
+        public void ExcluirTeste()
+        {
+            var destino = _db.Destinos.Find(1);
+
+            _db.Destinos.Remove(destino);
+
+            _db.SaveChanges();
+        }
+    }
+}
