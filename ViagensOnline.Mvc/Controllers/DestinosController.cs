41ea8a847f3687dcfecd2d03634055a8525210d1
Vítor Neil Avelino <vitor.avelino@concrete.com.br>
2018-08-14 15:50:56 -0300

    Listagem de destinos

---
+++ ViagensOnline.Mvc/Controllers/DestinosController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ViagensOnline.Repositorios.SqlServer;
+
+namespace ViagensOnline.Mvc.Controllers
+{
+    public class DestinosController : Controller
+    {
+        private ViagensOnlineDbContext _db = new ViagensOnlineDbContext();
+        // GET: Destinos
+        public ActionResult Index()
+        {
+            return View(_db.Destinos.ToList());
+        }
+    }
+}
